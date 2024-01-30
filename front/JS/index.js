1697dd57868206afa9c0fae8463d2be59655bccf
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-01-30 15:13:00 +0100

    Fetch request on index page

--- front/JS/index.js
+++ front/JS/index.js
@@ -1,1 +1,12 @@
-let couches = get.ElementById("items");
+const apiUrl = "http://localhost:3000/api/products";
+
+// function to populate front page//
+
+const fetchCouches = () => {
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error", error));
+};
+
+fetchCouches();
