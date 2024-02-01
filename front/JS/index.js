e7dbbac1f1a504c4056b7166f6bdaa18c9abbb16
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-02-01 16:45:37 +0100

    index.js completed

--- front/JS/index.js
+++ front/JS/index.js
@@ -1,12 +1,29 @@
 const apiUrl = "http://localhost:3000/api/products";
+let productContainer = document.getElementById("items");
 
-// function to populate front page//
-
-const fetchCouches = () => {
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error("Error", error));
+const fetchProducts = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+    displayProducts(data);
+  } catch (error) {
+    console.error("Error", error);
+  }
 };
 
-fetchCouches();
+function displayProducts(products) {
+  products.forEach((product) => {
+    const productCard = document.createElement("div");
+    productCard.innerHTML = `<a href="./product.html?id=${product._id}">
+             <article>
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>`;
+    productContainer.appendChild(productCard);
+  });
+}
+
+fetchProducts();
