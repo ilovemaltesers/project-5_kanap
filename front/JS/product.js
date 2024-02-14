c7a4825b27a09d76b78c849de59a4022d6469b12
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-02-14 15:54:42 +0100

    commenting product.js

--- front/JS/product.js
+++ front/JS/product.js
@@ -1,13 +1,26 @@
-const queryString = window.location.search;
-const params = new URLSearchParams(queryString);
-const couchId = params.get("id");
+// retrieves the query string portion of the current URL
+const urlSearch = window.location.search;
+
+// creates a new URLSearchParams object
+const urlParams = new URLSearchParams(urlSearch);
+
+// retrieves the value of the parameter "id" from the query string
+const couchId = urlParams.get("id");
+
+// constructs a URL for a product endpoint
 const productLink = `http://localhost:3000/api/products/${couchId}`;
 
+// to test if issue with couchId. (problem solving. will remove later)
 if (couchId) {
   fetchCouch();
 } else {
   console.error("No couch ID found in the URL");
 }
+
+/*asynchronous function to fetch data from the server including
+errors if fail. Function asks for the response in json stored in a 
+constant called couchData. The function also initialises the updateproductpage funtion
+with a catch error included */
 
 async function fetchCouch() {
   try {
@@ -22,6 +35,8 @@ async function fetchCouch() {
     console.error("Failed to update requested data:", error);
   }
 }
+
+// function retrieving elements from the Dom in order to display content
 
 function updateProductPage(couchData) {
   const couchImg = document.createElement("img");
