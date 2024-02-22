f72efe0f23369c44c4e4ad354cee091e61acf140
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-02-22 11:48:34 +0100

    Add to cart product.js

--- front/JS/product.js
+++ front/JS/product.js
@@ -1,10 +1,14 @@
+// retrieve query string portion of URL
 const keysValues = window.location.search;
 
 if (keysValues) {
+  // create new search params object
   const urlParams = new URLSearchParams(keysValues);
+  // retrieve value of query param id from url params object
   const id = urlParams.get("id");
 
   if (id) {
+    // contruct a url for api endpoint
     const productLink = `http://localhost:3000/api/products/${id}`;
 
     fetch(productLink)
@@ -16,28 +20,29 @@ if (keysValues) {
       })
       .catch((error) => {
         console.error("Error fetching product:", error);
-        // Handle error (e.g., show an error message to the user)
       });
   } else {
     console.error("No id parameter found in the URL");
-    // Handle the case where 'id' parameter is missing (e.g., redirect to an error page)
+    // Handle the case where 'id' parameter is missing
   }
   console.error("No URL parameters found");
-  // Handle the case where no URL parameters are present (e.g., redirect to an error page)
+  // Handle the case where no URL parameters are present
 }
+
+// function to populate page with product and its  attributes
 
 function updateProductPage(product) {
   const productImg = document.querySelector(".item__img");
   productImg.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
 
   const productTitle = document.getElementById("title");
-  productTitle.innerText += product.title;
+  productTitle.innerText = product.title;
 
   const productPrice = document.getElementById("price");
-  productPrice.innerText += product.price;
+  productPrice.innerText = product.price;
 
   const productDescription = document.getElementById("description");
-  productDescription.innerText += product.description;
+  productDescription.innerText = product.description;
 
   const productColors = document.getElementById("colors");
   product.colors.forEach((color) => {
@@ -50,8 +55,31 @@ function updateProductPage(product) {
 
 // Add to cart
 
-// const addToCartButton = document.getElementById("addToCart")
+const AddToCartButton = document.getElementById("addToCart");
 
-// addToCartButton.addEventListener('click')
+AddToCartButton.addEventListener("click", () => {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const selectedColor = document.getElementById("colors").value;
+  const selectedQuantity = parseInt(document.getElementById("quantity").value);
 
-// }
+  // Create a new product object in the cart
+  const newProduct = { id, selectedColor, selectedQuantity };
+
+  // Find existing product in the cart based on selected color
+  const existingProduct = cart.find(
+    (product) =>
+      product.selectedColor === newProduct.selectedColor &&
+      product.id === newProduct.id
+  );
+
+  if (existingProduct) {
+    // If existing product is found, update the quantity
+    existingProduct.selectedQuantity += newProduct.selectedQuantity;
+  } else {
+    // If no existing product is found, add the new product to the cart
+    cart.push(newProduct);
+  }
+  // Update the local storage with the modified cart
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Item Added to Cart");
+});
