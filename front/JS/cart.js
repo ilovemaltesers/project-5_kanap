e9e07418371fe4e12c985d2510d6c68941e86243
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-02-27 16:20:17 +0100

    empty cart redirect and display cart items in console

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -2,3 +2,13 @@
 
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
 console.log(cart);
+
+//if cart is empty then display a message to console and redirect to the homepage
+if (cart.length === 0) {
+  console.log("Cart is empty");
+  window.location.href = "/";
+}
+//if cart is not empty then display the cart items to the console
+else {
+  console.log("Cart items:", cart);
+}
