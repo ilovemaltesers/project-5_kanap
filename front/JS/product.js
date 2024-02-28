73d45415dc80853ab84b42025610f4ffd504c242
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-02-28 16:21:04 +0100

    function added to find existing prod then update

--- front/JS/product.js
+++ front/JS/product.js
@@ -1,9 +1,8 @@
 // Get the product id from the URL
 const urlParams = new URLSearchParams(window.location.search);
-
 const id = urlParams.get("id");
-// function to fetch product details from the API
 
+// Function to fetch product details from the API
 fetch(`http://localhost:3000/api/products/${id}`)
   .then((response) => response.json())
   .then((product) => {
@@ -13,8 +12,7 @@ fetch(`http://localhost:3000/api/products/${id}`)
     console.error("Error:", error);
   });
 
-// function to populate page with product and its attributes
-
+// Function to populate page with product and its attributes
 function updateProductPage(product) {
   const productImg = document.querySelector(".item__img");
   productImg.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
@@ -29,7 +27,6 @@ function updateProductPage(product) {
   productDescription.innerText = product.description;
 
   const productColors = document.getElementById("colors");
-
   product.colors.forEach((color) => {
     const option = document.createElement("option");
     option.value = color;
@@ -38,8 +35,30 @@ function updateProductPage(product) {
   });
 }
 
-// function to add product to cart only if certain conditions are met
+// Function to update the cart
 
+function updateCart(cartItem) {
+  // Get the current cart from local storage
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  // Find the index of the product in the cart that has the same id and colour
+  let index = cart.findIndex(
+    (item) => item.id === cartItem.id && item.color === cartItem.color
+  );
+
+  if (index !== -1) {
+    // If the product is already in the cart, update the quantity
+    cart[index].quantity += cartItem.quantity;
+  } else {
+    // If the product is not in the cart, add it
+    cart.push(cartItem);
+  }
+
+  // Save the updated cart back to local storage
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+// Function to add product to cart only if certain conditions are met
 window.onload = function () {
   const colorInput = document.getElementById("colors");
   const quantityInput = document.getElementById("quantity");
@@ -91,14 +110,8 @@ window.onload = function () {
         quantity: selectedQuantity,
       };
 
-      // Get the current cart from local storage
-      let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-      // Add the new item to the cart
-      cart.push(cartItem);
-
-      // Save the updated cart back to local storage
-      localStorage.setItem("cart", JSON.stringify(cart));
+      // Update the cart
+      updateCart(cartItem);
     }
   };
 };
