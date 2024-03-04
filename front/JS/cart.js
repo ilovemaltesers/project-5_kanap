1ac6494050cba1238f094f43255108d7ef170765
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-03-04 14:38:52 +0100

    testing out beginning of cart page

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,41 +1,47 @@
 const productsInLocalStorage = JSON.parse(localStorage.getItem("cart"));
 
-function updateCartPage(productsInLocalStorage) {
-  for (let i = 0; i < productsInLocalStorage.length; i++) {
-    const product = productsInLocalStorage[i];
+// Function to get one product from the API if located in localStorage
+const getProduct = async (id) => {
+  const response = await fetch(`http://localhost:3000/api/products/${id}`);
+  return response.json();
+};
 
-    // for each product in the local storage, create a new article in the cart
-
-    const section = document.getElementById("cart__items");
-    const article = document.createElement("article");
-
-    article.classList.add("cart__item");
-    article.setAttribute("data-id", product.id);
-    article.setAttribute("data-color", product.selectedColor);
-    article.setAttribute("data-price", product.price);
-    article.innerHTML = `
-      <div class="cart__item__img">
-        <img src="${product.imageUrl}" alt="${product.altTxt}">
-      </div>
-      <div class="cart__item__content">
-        <div class="cart__item__content__titlePrice">
-          <h2>${product.name}</h2>
-          <p>${product.selectedColor}</p>
-          <p>${product.price} €</p>
-        </div>
-        <div class="cart__item__content__settings">
-          <div class="cart__item__content__settings__quantity">
-            <p>Qté : </p>
-            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
-          </div>
-          <div class="cart__item__content__settings__delete">
-            <p class="deleteItem">Supprimer</p>
-          </div>
-        </div>
-      </div>
-    `;
-    section.appendChild(article);
-  }
+if (productsInLocalStorage) {
+  productsInLocalStorage.forEach((product) => {
+    getProduct(product._id).then((product) => {
+      displayProductInCart(product);
+    });
+  });
 }
 
-updateCartPage(productsInLocalStorage);
+// for each item in the cart we display using the html from cart.html page
+const displayProductInCart = (product) => {
+  const cartItems = document.getElementById("cart__items");
+  const item = document.createElement("article");
+  item.classList.add("cart__item");
+  item.dataset.id = product._id;
+  item.innerHTML = `
+    <div class="cart__item__img">
+      <img src="${product.imageUrl}" alt="${product.altTxt}">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item
+      _content__titlePrice">
+        <h2>${product.name}</h2>
+        <p>${product.price} €</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item
+        _content__settings__quantity">
+          <p>Qté : </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="1">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem
+          ">Supprimer</p>
+        </div>
+      </div>
+    </div>
+  `;
+  cartItems.appendChild(item);
+};
