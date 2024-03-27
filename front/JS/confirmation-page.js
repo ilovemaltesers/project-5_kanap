461b33efc73af52f6a38817dc49946a1ee64de55
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-03-27 13:15:58 +0100

    removed console logs from confirmation page to reduce code

--- front/JS/confirmation-page.js
+++ front/JS/confirmation-page.js
@@ -1,10 +1,6 @@
 let url = new URL(window.location.href);
 
-console.log(url);
-
 let orderId = url.searchParams.get("id");
-
-console.log(orderId);
 
 let confirmOrderId = document.getElementById("orderId");
 
