5f1a4fad0fd203e1f0e3eb75e7175c7db51af271
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-03-27 13:14:15 +0100

    confirmation page

--- front/JS/confirmation-page.js
+++ front/JS/confirmation-page.js
@@ -1,1 +1,11 @@
+let url = new URL(window.location.href);
 
+console.log(url);
+
+let orderId = url.searchParams.get("id");
+
+console.log(orderId);
+
+let confirmOrderId = document.getElementById("orderId");
+
+confirmOrderId.innerHTML = orderId;
