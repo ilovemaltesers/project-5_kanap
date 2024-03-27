d219f7035af40e7f1f69f6774c3cdab04622de4e
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-03-27 14:02:24 +0100

    comments confirmation page

--- front/JS/confirmation-page.js
+++ front/JS/confirmation-page.js
@@ -1,7 +1,13 @@
 let url = new URL(window.location.href);
 
+// retrieve the value of the query parameter named "id" from url
+
 let orderId = url.searchParams.get("id");
 
+// retrieve the element with the id "orderId" from the DOM
+
 let confirmOrderId = document.getElementById("orderId");
+
+// set the innerHTML of the element to the value of the orderId
 
 confirmOrderId.innerHTML = orderId;
